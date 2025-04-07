cf4c1f2a192321b8e067c99725bcdf2fbc77e5b5
kage1020 <4624528@ed.tus.ac.jp>
2025-04-08 00:02:27 +0900

    fix: page params provider should use in page.tsx

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,3 +1,5 @@
+import { defaultPlaylistOption } from '@/libs/const';
+import { getTranslation } from '@/libs/i18n';
 import { type PageParams, parsePageParams } from '@/libs/params';
 import { getMediaList, getPlaylist } from '@/libs/playlist';
 import { Menu } from './_components/menu';
@@ -5,9 +7,11 @@ import { Player } from './_components/player';
 import { PlaylistDrawer } from './_components/drawer/playlist';
 import { SettingsDrawer } from './_components/drawer/settings';
 import { OptionModal } from './_components/modal';
+import { Provider } from './_components/provider';
 
-export default async function Home({ searchParams }: PageParams) {
-  const { parsedSearchParams } = await parsePageParams({ searchParams });
+export default async function Home({ params, searchParams }: PageParams) {
+  const { parsedParams, parsedSearchParams } = await parsePageParams({ params, searchParams });
+  const t = await getTranslation(parsedParams.locale);
 
   const playlist = parsedSearchParams.playlist
     ? await getPlaylist(parsedSearchParams.playlist)
@@ -22,12 +26,16 @@ export default async function Home({ searchParams }: PageParams) {
     : [];
 
   return (
-    <>
+    <Provider
+      t={t}
+      pageParams={{ parsedParams, parsedSearchParams }}
+      playlistOption={playlist?.options ?? defaultPlaylistOption}
+    >
       <Player playlist={playlist} mediaList={mediaList} />
       <Menu playlist={playlist} mediaList={mediaList} />
       <PlaylistDrawer mediaList={mediaList} />
       <SettingsDrawer />
       <OptionModal />
-    </>
+    </Provider>
   );
 }
